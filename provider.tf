b61a45db085e0de804299e7392da6ad19c516073
shankarajmeera <37999721+shankarajmeera@users.noreply.github.com>
2022-04-11 11:43:09 +0530

    Update provider.tf
    updated with latest config

--- provider.tf
+++ provider.tf
@@ -1,3 +1,17 @@
+terraform {
+	required_providers {
+		aws = {
+			source  = "hashicorp/aws"
+			version = "~> 3.74"
+		}
+	}
+}
+
+provider "aws" {
+	region = "us-west-2"
+}
+/*
 provider "aws" {
   region = "ap-south-1"
 }
+*/
